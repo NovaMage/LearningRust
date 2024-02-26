91acb7cf6535b75bdaed5151ae89ed059a682fdf
Angel Blanco <angel.softworks@gmail.com>
2024-02-25 23:52:32 -0400

    Worked with module organization, let's gooooo

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -1,5 +1,24 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
     let my_string = String::from("hello world");
+
+    let dime_in_cents = value_in_cents(Coin::Dime);
+    println!("The value in cents of a dime is {dime_in_cents}");
 
     // `first__word` works on slices of `String`s, whether partial or whole
     let _word = first_word(&my_string[0..6]);
@@ -19,6 +38,18 @@ fn main() {
     let _word = first_word(my_string_literal);
 
     println!("The _word is {_word}");
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    let value: Option<i32> = Some(5);
+
+    let t = value.unwrap_or_else(|| 5);
+    let _target_none: Option<u32> = None;
+    println!("My option is {}", value.unwrap());
+
+    assert_eq!(slice, &[2, 3]);
 }
 
 fn first_word(s: &str) -> &str {
