2d4ec5f4011751ed47bb12d8f94d2efab446b94b
Angel Blanco <angel.softworks@gmail.com>
2024-05-09 20:54:38 -0400

    Updating hello cargo

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,29 +1,25 @@
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
+const N: usize = 10;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
+    println!("{}", find_duplicate([1, 2, 8, 3, 4, 5, 6, 7, 8, 9]))
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+fn find_duplicate(nums: [usize; N]) -> usize {
+    let mut tortoise = nums[0];
+    let mut hare = nums[0];
+    loop {
+        tortoise = nums[tortoise];
+        hare = nums[nums[hare]];
+        if tortoise == hare {
+            break;
+        }
+    }
 
-    println!("Request: {:#?}", http_request);
-
-    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello world";
-
-    stream.write_all(response.as_bytes()).unwrap();
+    let mut ptr1 = nums[0];
+    let mut ptr2 = tortoise;
+    while ptr1 != ptr2 {
+        ptr1 = nums[ptr1];
+        ptr2 = nums[ptr2];
+    }
+    ptr1
 }
